98ff88d50b7873853feb3f9bf612953737bf19ff
Alex <mralexblood@outlook.com>
2024-12-04 08:39:34 +0800

    Day 3 part 2 done!

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -7,8 +7,11 @@ fn main() {
     let _ = file
         .expect("failed to read file")
         .read_to_string(&mut content);
-    let part1 = part_1(content);
-    println!("{}", part1)
+    let part1 = part_1(content.clone());
+    println!("{}", part1);
+
+    let part2 = part_2(content);
+    println!("{}", part2);
 }
 fn part_1(content: String) -> i32 {
     let mut total = 0;
@@ -16,7 +19,6 @@ fn part_1(content: String) -> i32 {
     for matches in regex.find_iter(content.as_str()) {
         let mut nums = matches.as_str().to_string();
         nums.retain(|c| c != 'm' && c != 'u' && c != 'l' && c != '(' && c != ')');
-        //        println!("{}", nums);
         if nums.len() > 1 {
             let mut mult = 1;
             for num in nums.split(",").into_iter() {
@@ -25,6 +27,16 @@ fn part_1(content: String) -> i32 {
             }
             total += mult;
         }
+    }
+    total
+}
+
+fn part_2(content: String) -> i32 {
+    let mut total = 0;
+    let do_split = content.split("do()").into_iter();
+    for does in do_split {
+        let mut dont_split = does.split("don't()");
+        total += part_1(dont_split.next().unwrap().to_string());
     }
     total
 }
