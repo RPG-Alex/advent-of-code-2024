aa522ec3c7dc0bc540d08040b2c63db06974e448
Alex <mralexblood@outlook.com>
2024-12-05 16:35:43 +0800

    Started Day 5

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -64,6 +64,41 @@ fn part_1(content: String) -> i32 {
 
 fn part_2(content: String) -> i32 {
     let mut total = 0;
+    let grid: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
+    let directions: [(isize, isize); 4] = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+
+    let word = "MAS";
+    let word_chars: Vec<char> = word.chars().collect();
+    let word_len = word_chars.len() as isize;
+
+    let height = grid.len() as isize;
+    let width = grid[0].len() as isize;
+
+    for i in 0..height {
+        for j in 0..width {
+            for (dx, dy) in directions {
+                let mut match_found = true;
+                // gotta change this part. Start from finding an A and then run algorithm.
+                for k in 0..word_len {
+                    let new_i = i + dx * k;
+                    let new_j = j + dy * k;
+
+                    if new_i < 0 || new_j < 0 || new_i >= height || new_j >= width {
+                        match_found = false;
+                        break;
+                    }
+
+                    if grid[new_i as usize][new_j as usize] != word_chars[k as usize] {
+                        match_found = false;
+                        break;
+                    }
+                }
+                if match_found {
+                    total += 1;
+                }
+            }
+        }
+    }
 
     total
 }
--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,1 +1,85 @@
-fn main() {}
+use std::{fs::File, io::Read};
+
+struct Parts {
+    rule: String,
+    pages: String,
+}
+
+fn main() {
+    let file = File::open("inputs/day5_1");
+    let mut content = String::new();
+    let _ = file
+        .expect("failed to read file")
+        .read_to_string(&mut content);
+    let part1 = part_1(content.clone());
+    println!("{}", part1);
+
+    let part2 = part_2(content);
+    println!("{}", part2);
+}
+
+fn part_1(content: String) -> i32 {
+    let mut total = 0;
+    let rule_pages = rule_break(content);
+    let mut rules: Vec<(i32, i32)> = rules_helper(rule_pages.rule);
+    let mut pages: Vec<Vec<i32>> = pages_helper(rule_pages.pages);
+
+    total
+}
+
+fn part_2(content: String) -> i32 {
+    let mut total = 0;
+
+    total
+}
+
+fn rules_helper(content: String) -> Vec<(i32, i32)> {
+    let mut rules: Vec<(i32, i32)> = Vec::new();
+    let _: Vec<_> = content
+        .lines()
+        .map(|line| {
+            if line.contains("|") {
+                let mut line = line.split("|");
+                rules.push((
+                    line.next().unwrap().parse::<i32>().unwrap(),
+                    line.next().unwrap().parse::<i32>().unwrap(),
+                ));
+            }
+        })
+        .collect();
+    rules
+}
+fn pages_helper(content: String) -> Vec<Vec<i32>> {
+    let mut pages: Vec<Vec<i32>> = Vec::new();
+    let _: Vec<_> = content
+        .lines()
+        .map(|line| {
+            if line.contains(",") {
+                let mut line = line.split(",");
+                let mut page_line: Vec<i32> = Vec::new();
+                for p in line.into_iter() {
+                    page_line.push(p.parse::<i32>().unwrap());
+                }
+                pages.push(page_line);
+            }
+        })
+        .collect();
+    pages
+}
+
+fn rule_break(content: String) -> Parts {
+    let mut broken: Parts = Parts {
+        pages: "".to_string(),
+        rule: "".to_string(),
+    };
+
+    for (i, line) in content.lines().into_iter().enumerate() {
+        if !line.contains("|") && !line.contains(",") {
+            broken.rule = content[0..i - 1].to_string();
+            broken.pages = content[i + 1..content.len()].to_string();
+            break;
+        }
+    }
+
+    broken
+}
